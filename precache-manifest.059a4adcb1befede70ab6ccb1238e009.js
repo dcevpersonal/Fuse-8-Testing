cf02512cf97c351d3c537a7b655fe5c0c38467c2
Daniel Lane <dcevpersonal@gmail.com>
2021-03-16 21:08:10 +0500

    Web App

---
+++ precache-manifest.059a4adcb1befede70ab6ccb1238e009.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e2a6511c35a291d2792",
+    "url": "css/about.e574a52f.css"
+  },
+  {
+    "revision": "b9143a6b804eb06c9696",
+    "url": "css/app.948a95b8.css"
+  },
+  {
+    "revision": "4807bd12c4c64f05407eb3b77c13d613",
+    "url": "index.html"
+  },
+  {
+    "revision": "5e2a6511c35a291d2792",
+    "url": "js/about.4a08a627.js"
+  },
+  {
+    "revision": "b9143a6b804eb06c9696",
+    "url": "js/app.0f00ff06.js"
+  },
+  {
+    "revision": "7960823ca564fb64137f",
+    "url": "js/chunk-vendors.c84d6719.js"
+  },
+  {
+    "revision": "1735e83ccd1bf5210561372313d4b8c5",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
